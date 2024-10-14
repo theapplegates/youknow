b62dc4ee6bcb6c0d4911e0c2a33f697cb3d989cc
Shu Ding <g@shud.in>
2024-10-14 17:18:55 +0200

    wip

---
+++ components/project-logos.tsx
@@ -0,0 +1,172 @@
+import {
+  ArrowsRightLeftIcon,
+  Bars3BottomLeftIcon,
+  CloudIcon,
+  CodeBracketSquareIcon,
+  CpuChipIcon,
+  GlobeAsiaAustraliaIcon,
+  HashtagIcon,
+  SparklesIcon,
+} from '@heroicons/react/16/solid'
+
+export function Nextjs({ height = 24 }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 394 79'
+      height={height}
+      xlinkTitle='Next.js'
+    >
+      <path d='M262 0h68.5v12.7h-27.2v66.6h-13.6V12.7H262V0zM149 0v12.7H94v20.4h44.3v12.6H94v21h55v12.6H80.5V0h68.7zm34.3 0h-17.8l63.8 79.4h17.9l-32-39.7 32-39.6h-17.9l-23 28.6-23-28.6zm18.3 56.7l-9-11-27.1 33.7h17.8l18.3-22.7z'></path>
+      <path
+        fillRule='evenodd'
+        d='M81 79.3L17 0H0v79.3h13.6V17l50.2 62.3H81z'
+        clipRule='evenodd'
+      ></path>
+      <path d='M333.6 78.9c-1 0-1.8-.4-2.5-1-.8-.8-1.1-1.6-1.1-2.6s.3-1.8 1-2.5c.8-.7 1.6-1 2.6-1s1.8.3 2.5 1a3.4 3.4 0 01.6 4.3 3.7 3.7 0 01-3 1.8zm23.2-33.5h6v23.3c0 2.1-.4 4-1.3 5.5a9.1 9.1 0 01-3.8 3.5c-1.6.8-3.5 1.3-5.7 1.3-2 0-3.7-.4-5.3-1a8.5 8.5 0 01-3.7-3.2c-.9-1.3-1.4-3-1.4-5h6c.1.8.3 1.6.7 2.2.4.7 1 1.2 1.6 1.5.7.4 1.5.5 2.4.5 1 0 1.8-.2 2.4-.6a4 4 0 001.6-1.8c.3-.8.5-1.8.5-3V45.5zm30.9 9.1a4.4 4.4 0 00-2-3.3 7.5 7.5 0 00-4.3-1.1c-1.3 0-2.4.2-3.3.5-.9.4-1.6 1-2 1.6a3.5 3.5 0 00-.3 4c.3.5.7.9 1.3 1.2l1.8 1 2.1.5 3.1.8c1.3.3 2.5.7 3.7 1.2a13 13 0 013.2 1.8 8.1 8.1 0 013 6.5c0 2-.5 3.7-1.5 5.1a10 10 0 01-4.4 3.5c-1.8.8-4.1 1.2-6.8 1.2-2.6 0-4.9-.4-6.8-1.2-2-.8-3.4-2-4.5-3.5a10 10 0 01-1.7-5.6h6a5 5 0 003.5 4.6c1 .4 2.2.6 3.4.6 1.3 0 2.5-.2 3.5-.6 1-.4 1.8-1 2.4-1.7a4 4 0 00.8-2.4c0-.9-.2-1.6-.7-2.2a11 11 0 00-2.1-1.4l-3.2-1-3.8-1c-2.8-.7-5-1.7-6.6-3.2a7.2 7.2 0 01-2.4-5.7 8 8 0 011.7-5 10 10 0 014.3-3.5c2-.8 4-1.2 6.4-1.2 2.3 0 4.4.4 6.2 1.2 1.8.8 3.2 2 4.3 3.4 1 1.4 1.5 3 1.5 5h-5.8z'></path>
+    </svg>
+  )
+}
+
+export function V0({ height = 24 }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 40 20'
+      height={height}
+      xlinkTitle='Vercel v0'
+    >
+      <path d='M23.4 0h9.5a7 7 0 017 7v9H36V6.8l-9.5 9.4H36v3.7h-9.4a7 7 0 01-7.1-7V3.7h3.9v9.6l9.8-9.6h-9.8V0zm-9.6 19.1L0 3.7h5.5l8.1 9v-9h4.2v13.9c0 2-2.6 3-4 1.5z'></path>
+    </svg>
+  )
+}
+
+export function Nextra({ height = 24 }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='25 30 370 70'
+      height={height}
+      xlinkTitle='Nextra'
+    >
+      <path
+        fill='#000'
+        d='M144.91 63.28v28.76h-11.57V42.95h11.06v8.34h.58a14.03 14.03 0 015.4-6.55c2.5-1.62 5.57-2.43 9.24-2.43 3.38 0 6.33.72 8.85 2.17a14.64 14.64 0 015.88 6.3c1.4 2.75 2.1 6.08 2.08 10v31.26h-11.57V62.57c0-3.28-.86-5.85-2.56-7.7-1.68-1.85-4.02-2.78-7-2.78-2.02 0-3.82.45-5.4 1.34a9.3 9.3 0 00-3.68 3.8 12.85 12.85 0 00-1.3 6.05zM206.32 93c-4.92 0-9.17-1.02-12.75-3.07a20.78 20.78 0 01-8.22-8.76c-1.91-3.79-2.87-8.25-2.87-13.39 0-5.05.96-9.48 2.87-13.3a21.75 21.75 0 018.12-8.94c3.47-2.15 7.56-3.23 12.24-3.23 3.03 0 5.88.49 8.57 1.47 2.7.96 5.1 2.45 7.16 4.48a20.98 20.98 0 014.92 7.73c1.2 3.11 1.79 6.82 1.79 11.12v3.55H187.9v-7.8h29.15c-.02-2.22-.5-4.19-1.44-5.91a10.46 10.46 0 00-3.93-4.12c-1.66-1-3.6-1.5-5.82-1.5-2.36 0-4.44.57-6.23 1.72a11.89 11.89 0 00-4.19 4.47 12.87 12.87 0 00-1.5 6.04v6.81a16 16 0 001.57 7.35 11.17 11.17 0 004.38 4.67 13.1 13.1 0 006.58 1.6 14 14 0 004.57-.7c1.36-.5 2.55-1.21 3.55-2.15s1.76-2.1 2.27-3.48l10.8 1.21a16.84 16.84 0 01-3.9 7.48 19.01 19.01 0 01-7.29 4.92A27.92 27.92 0 01206.32 93zm34.78-50.05l9.9 18.12 10.07-18.12h12.24l-14.8 24.55 15.06 24.54h-12.18l-10.38-17.67-10.3 17.67h-12.27L243.4 67.5l-14.58-24.55h12.28zm63.88 0v8.95h-28.23v-8.95h28.23zm-21.26-11.76h11.57v46.08c0 1.56.24 2.75.7 3.58a3.8 3.8 0 001.92 1.67c.79.3 1.66.44 2.62.44a16.47 16.47 0 003.39-.44l1.95 9.04a21.73 21.73 0 01-6.8 1.15c-2.9.09-5.52-.35-7.84-1.3a12.18 12.18 0 01-5.53-4.55c-1.34-2.04-2-4.6-1.98-7.67v-48zm29.13 60.85V42.95h11.22v8.18h.51a12.24 12.24 0 0112.11-8.88c1.32 0 2.54.16 3.65.48 1.1.32 2.09.75 2.94 1.3l-3.58 9.6a10.15 10.15 0 00-4.57-1.02c-2 0-3.82.44-5.44 1.33-1.62.9-2.9 2.15-3.86 3.74a10.54 10.54 0 00-1.4 5.5v28.86h-11.58zm51.22.86c-3.85 0-7.3-.99-10.35-2.97a20.2 20.2 0 01-7.23-8.63c-1.76-3.77-2.65-8.35-2.65-13.74 0-5.46.9-10.06 2.69-13.8 1.8-3.78 4.25-6.62 7.32-8.54a18.8 18.8 0 0110.25-2.91c2.88 0 5.25.49 7.1 1.47a13.4 13.4 0 014.41 3.48 21.3 21.3 0 012.53 3.8h.48v-8.11h11.6v49.09h-11.38v-7.73h-.7a19.8 19.8 0 01-2.6 3.8 14.24 14.24 0 01-4.47 3.39c-1.85.93-4.18 1.4-7 1.4zm3.23-9.49a10 10 0 006.27-1.98 12.4 12.4 0 003.93-5.6 23.7 23.7 0 001.34-8.33c0-3.18-.45-5.94-1.34-8.28a11.78 11.78 0 00-3.9-5.47c-1.7-1.3-3.8-1.95-6.3-1.95-2.58 0-4.73.67-6.45 2.02a12.2 12.2 0 00-3.9 5.56 23.35 23.35 0 00-1.31 8.12 24 24 0 001.3 8.2c.9 2.4 2.21 4.28 3.94 5.67a10.12 10.12 0 006.42 2.04z'
+      ></path>
+      <path
+        fill='#000'
+        stroke='#000'
+        strokeWidth='2'
+        d='M98.27 35.2a2.4 2.4 0 00-3.39-3.39l-2.84 2.85a38.18 38.18 0 01-54 0l-2.84-2.85a2.4 2.4 0 00-3.39 3.39l2.85 2.84a38.18 38.18 0 010 54l-2.85 2.84a2.4 2.4 0 103.39 3.39l2.84-2.85a38.18 38.18 0 0154 0l2.84 2.85a2.4 2.4 0 103.39-3.39l-2.85-2.84a38.18 38.18 0 010-54l2.85-2.84z'
+      ></path>
+    </svg>
+  )
+}
+
+export function SWR({ height = 24 }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 275 65'
+      height={height}
+      xlinkTitle='SWR'
+    >
+      <path
+        fill='#000'
+        d='M.35 34.73C.42 51.36 13.98 65 30.63 65a30.3 30.3 0 0030.28-30.27v-4.28a12.48 12.48 0 0122.13-7.86h19.57A30.33 30.33 0 0073.37.17 30.31 30.31 0 0043.1 30.45v4.28a12.48 12.48 0 01-24.93 0c0-1.97-1.6-3.57-3.57-3.57H3.92a3.56 3.56 0 00-3.57 3.57zM116.2 65a30.4 30.4 0 01-29.21-22.41h19.54a12.45 12.45 0 0022.14-7.87v-4.27A30.31 30.31 0 01158.94.17a30.31 30.31 0 0130.28 29.63l.09 4.93a12.48 12.48 0 0022.13 7.86h19.57A30.33 30.33 0 01201.78 65c-16.66 0-30.21-13.64-30.28-30.27l-.1-4.45v-.18a12.48 12.48 0 00-12.46-12.12 12.5 12.5 0 00-12.47 12.47v4.28A30.3 30.3 0 01116.2 65zm118.65-42.41a12.45 12.45 0 0122.13 7.86c0 1.97 1.6 3.56 3.57 3.56h10.68c1.97 0 3.56-1.6 3.56-3.56A30.31 30.31 0 00244.52.17a30.33 30.33 0 00-29.24 22.42h19.57z'
+      ></path>
+    </svg>
+  )
+}
+
+function LogoWithIcon({
+  icon,
+  title,
+}: {
+  icon: React.ReactNode
+  title: string
+}) {
+  return (
+    <span
+      className='inline-flex items-center gap-1 whitespace-nowrap select-none text-black overflow-hidden'
+      title={title}
+    >
+      <span className='shrink-0 min-w-0'>{icon}</span>
+      <span
+        className='text-[16px] font-bold tracking-[-0.005em] leading-none mt-px'
+        style={{
+          // Use smaller opsz to make the logo more readable. Also make it more stylized by turning on
+          // flags like cv11.
+          fontVariationSettings: '"wght" 700, "opsz" 14',
+          fontFeatureSettings:
+            '"dlig" 1, "cv01", "cv03", "cv04", "cv05", "cv08", "cv09", "cv11", "calt", "ss03", "cpsp", "liga", "ordn"',
+        }}
+      >
+        {title}
+      </span>
+    </span>
+  )
+}
+
+export function AISDK({ height = 24 }) {
+  return (
+    <LogoWithIcon
+      icon={<SparklesIcon height={height} />}
+      title='Vercel AI SDK'
+    />
+  )
+}
+
+export function COBE({ height = 24 }) {
+  return (
+    <LogoWithIcon
+      icon={<GlobeAsiaAustraliaIcon height={height} />}
+      title='COBE'
+    />
+  )
+}
+
+export function ReactWrapBalancer({ height = 24 }) {
+  return (
+    <LogoWithIcon
+      icon={<Bars3BottomLeftIcon height={height} />}
+      title='React Wrap Balancer'
+    />
+  )
+}
+
+export function Satori({ height = 24 }) {
+  return (
+    <LogoWithIcon
+      icon={<CodeBracketSquareIcon height={height} />}
+      title='Satori'
+    />
+  )
+}
+
+export function Mak({ height = 24 }) {
+  return <LogoWithIcon icon={<HashtagIcon height={height} />} title='mak.ink' />
+}
+
+export function NextViewTransitions({ height = 24 }) {
+  return (
+    <LogoWithIcon
+      icon={<ArrowsRightLeftIcon height={height} />}
+      title='Next View Transitions'
+    />
+  )
+}
+
+export function Y86Simulator({ height = 24 }) {
+  return (
+    <LogoWithIcon
+      icon={<CpuChipIcon height={height} />}
+      title='Y86 CPU Simulator'
+    />
+  )
+}
+
+export function Metaballs({ height = 24 }) {
+  return <LogoWithIcon icon={<CloudIcon height={height} />} title='Metaballs' />
+}
