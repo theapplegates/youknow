42b0ef94c5fd1bcd6fb02ee566e6942eb4ee221f
Shu Ding <g@shud.in>
2024-12-31 13:23:45 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,18 +55,20 @@ export default function RootLayout({
             sans.variable,
             serif.variable,
             mono.variable,
-            'container p-6 sm:p-10 md:p-14',
+            'w-full p-6 sm:p-10 md:p-14',
             'text-sm leading-6 sm:text-[15px] sm:leading-7 md:text-base md:leading-7',
             'text-rurikon-500',
             'antialiased'
           )}
         >
           <div className='fixed sm:hidden h-6 sm:h-10 md:h-14 w-full top-0 left-0 z-30 pointer-events-none content-fade-out' />
-          <div className='flex'>
+          <div className='flex flex-col mobile:flex-row'>
             <Navbar />
             <main className='relative flex-1 max-w-2xl [contain:inline-size]'>
-              <div className='absolute w-px h-full bg-rurikon-border left-0' />
-              <article className='pl-6 sm:pl-10 md:pl-14'>{children}</article>
+              <div className='absolute w-full h-px opacity-50 bg-rurikon-border right-0 mobile:right-auto mobile:left-0 mobile:w-px mobile:h-full mobile:opacity-100' />
+              <article className='pl-0 pt-6 mobile:pt-0 mobile:pl-6 sm:pl-10 md:pl-14'>
+                {children}
+              </article>
             </main>
           </div>
         </body>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -31,8 +31,8 @@ function Item(props: React.ComponentProps<typeof Link>) {
 
 export default function Navbar() {
   return (
-    <nav className='mr-6 sm:mr-10 md:mr-14 w-16'>
-      <ul className='lowercase text-right sticky top-6 sm:top-10 md:top-14'>
+    <nav className='mobile:mr-6 sm:mr-10 md:mr-14 w-full mobile:w-16'>
+      <ul className='lowercase text-right mobile:sticky top-6 sm:top-10 md:top-14 mb-6 mobile:mb-0 flex gap-2 justify-end mobile:block'>
         <Item href='/'>About</Item>
         <Item href='/thoughts'>Thoughts</Item>
         {/* <Item href='/visuals'>Visuals</Item> */}
